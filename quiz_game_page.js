ad03870167e4093c970c03e0b3071429c16564bc
soumyasrishti01 <78192188+soumyasrishti01@users.noreply.github.com>
2021-05-28 19:38:32 +0530

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,10 @@
+function changescreen(){
+    window.location = "2ndindex.html";
+}
+function addUser(){
+    document.getElementById("p_1_text").value;
+    document.getElementById("p_2_text").value;
+
+    localStorage.setItem("p_1_text");
+    localStorage.setItem("p_2_text");
+}
